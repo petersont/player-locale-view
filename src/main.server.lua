83684afc0218223405b0d33aa665ccb99d4d9071
petersont <peterson.trethewey@gmail.com>
2018-08-03 14:04:06 -0700

    propagate Window

--- src/main.server.lua
+++ src/main.server.lua
@@ -10,31 +10,32 @@ Window.Title = "Localization Testing"
 
 canAccess, result = pcall(function() return game.LocalizationService.RobloxForcePlayModeRobloxLocaleId end)
 
-local params
 if canAccess then
-    local Localization = game:GetService("LocalizationService")
-    params = {
-        SetRobloxLocaleId = function(localeId) game.LocalizationService.RobloxForcePlayModeRobloxLocaleId = localeId end,
-        SetGameLocaleId = function(localeId) game.LocalizationService.RobloxForcePlayModeGameLocaleId = localeId end,
-        InitialRobloxLocaleId = game.LocalizationService.RobloxForcePlayModeRobloxLocaleId,
-        InitialGameLocaleId = game.LocalizationService.RobloxForcePlayModeGameLocaleId,
-    }
+	local Localization = game:GetService("LocalizationService")
+	params = {
+		Window = Window,
+		SetRobloxLocaleId = function(localeId) game.LocalizationService.RobloxForcePlayModeRobloxLocaleId = localeId end,
+		SetGameLocaleId = function(localeId) game.LocalizationService.RobloxForcePlayModeGameLocaleId = localeId end,
+		InitialRobloxLocaleId = game.LocalizationService.RobloxForcePlayModeRobloxLocaleId,
+		InitialGameLocaleId = game.LocalizationService.RobloxForcePlayModeGameLocaleId,
+	}
 else
-    local test_gameLocaleId = "en-us"
-    local test_robloxLocaleId = "en-us"
-    params = {
-        SetRobloxLocaleId = function(localeId) test_robloxLocaleId = localeId end,
-        SetGameLocaleId = function(localeId) test_gameLocaleId = localeId end,
-        InitialRobloxLocaleId = test_robloxLocaleId,
-        InitialGameLocaleId = test_gameLocaleId,
-    }
+	local test_gameLocaleId = "en-us"
+	local test_robloxLocaleId = "en-us"
+	params = {
+		Window = Window,
+		SetRobloxLocaleId = function(localeId) test_robloxLocaleId = localeId end,
+		SetGameLocaleId = function(localeId) test_gameLocaleId = localeId end,
+		InitialRobloxLocaleId = test_robloxLocaleId,
+		InitialGameLocaleId = test_gameLocaleId,
+	}
 end
 
 Roact.mount(Roact.createElement(PlayerLocaleView, params), Window)
 
 Button.Click:Connect(
-    function()
-        Window.Enabled = not Window.Enabled
-    end
+	function()
+		Window.Enabled = not Window.Enabled
+	end
 )
 
